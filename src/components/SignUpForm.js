04ee354005c1486bf97ab094c0159c5f875101c3
imankd <akhondiiman@gmail.com>
2022-09-17 00:34:51 +0430

    Add Formik and Yup to the project

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,25 +1,78 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validationSchema=Yup.object({
+    name:Yup.string().required("name is required").min(6,"at least 6 characters"),
+    email:Yup.string().email().required("email is required"),
+    password:Yup.string().required("password is required")
+})
+
 const SignUpForm = () => {
-    return ( 
-        <div className="u-section">
-        <div className="u-container-layout">
-        <form>
-            <div className="formControl">
-                <label>name</label>
-                <input type="text" />
-            </div>
-            <div className="formControl">
-                <label>email</label>
-                <input type="text" />
-            </div>
-            <div className="formControl">
-                <label>password</label>
-                <input type="text" />
-            </div>
-            <button type="submit">add</button>
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
+  console.log(formik.touched);
+
+  return (
+    <div className="u-section">
+      <div className="u-container-layout">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="formControl">
+            <label>name</label>
+            <input
+              type="text"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.name && formik.touched.name && (
+              <div className="error">{formik.errors.name}</div>
+            )}
+          </div>
+          <div className="formControl">
+            <label>email</label>
+            <input
+              type="text"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.email && formik.touched.email && (
+              <div className="error">{formik.errors.email}</div>
+            )}
+          </div>
+          <div className="formControl">
+            <label>password</label>
+            <input
+              type="text"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.password && formik.touched.password && (
+              <div className="error">{formik.errors.password}</div>
+            )}
+          </div>
+          <button type="submit">add</button>
         </form>
-        </div>
-        </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default SignUpForm;
